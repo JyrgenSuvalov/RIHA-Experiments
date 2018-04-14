18836dc736bf7e4c15ea66c1bb4f454566277084
Hannes Kiivet <hannes.kiivet@ria.ee>
2018-04-14 15:10:01 +0300

    Update and rename X-Road-fact.js to X-Road-fact/calc.js

--- X-Road-fact/calc.js
+++ X-Road-fact/calc.js
@@ -14,9 +14,9 @@ function compare(a, b) {
 }
 
 // Loads data from V5
-$.getJSON("X-Road-data.js", function(data_v5) {
+$.getJSON("v5.json", function(data_v5) {
     // Loads data from V6
-    $.getJSON("X-Road-data-v6.js", function(data_v6) {
+    $.getJSON("v6.json", function(data_v6) {
         // Set up variables
         var final_object = data_v5;
         var addition_list = ["secsrv_count", "service_count", "subsystem_count", "query_count"];
